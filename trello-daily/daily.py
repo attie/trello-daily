8210c4a6b1427a11c411c441c3244c1a8f5ad3e8
Attie Grande <attie@attie.co.uk>
2018-10-28 12:56:33 +0000

    split into functions

--- trello-daily/daily.py
+++ trello-daily/daily.py
@@ -18,6 +18,12 @@ class daily:
         board = self.tclient.get_board(oracle['daily']['board_id'])
         dates = [ *self.get_date_range() ]
 
+        fallback_list = self.validate_fallback_list(board)
+        self.close_old_lists(board, fallback_list, dates)
+        self.create_new_lists(board, dates)
+        self.order_lists(board, fallback_list, dates)
+
+    def validate_fallback_list(self, board):
         if oracle['daily']['fallback_list_id'] not in [ _.id for _ in board.all_lists() ]:
             raise Exception('fallback list is missing...')
 
@@ -27,6 +33,9 @@ class daily:
             print('Re-Open Fallback List...')
             fallback_list.open()
 
+        return fallback_list
+
+    def close_old_lists(self, board, fallback_list, dates):
         for l in board.open_lists():
             if l.id == oracle['daily']['fallback_list_id']:
                 continue
@@ -38,6 +47,7 @@ class daily:
             l.move_all_cards(fallback_list)
             l.close()
 
+    def create_new_lists(self, board, dates):
         lists = board.open_lists()
         for date, date_str, date_today in dates:
             if date_str in [ _.name for _ in lists ]:
@@ -46,6 +56,7 @@ class daily:
             print('Create List [%s]...' % date_str )
             board.add_list(date_str)
 
+    def order_lists(self, board, fallback_list, dates):
         lists = board.open_lists()
         for i, date, date_str, date_today in [ ( _ + 2, __[0], __[1], __[2] ) for _, __ in enumerate(sorted(dates, key=lambda d: d[0])) ]:
             for l in [ _ for _ in lists if _.name == date_str ]:
